3cd80456b51ca9c035d845ec770dd0491656c7cf
dltndn <james98099@gmail.com>
2023-03-12 14:34:56 +0900

    style: 회차 학습 버튼 생성

--- app/page/ManageContents.js
+++ app/page/ManageContents.js
@@ -1,24 +1,48 @@
-import { StyleSheet, View, Pressable, Text } from "react-native";
+import { StyleSheet, View, Pressable, Text, ScrollView } from "react-native";
 import MainLogo from "../components/MainLogo";
+import { roundData } from "../roundMockData";
+
+// 배열
+const mockData = roundData;
 
 export default function ManageContents() {
   return (
     <View style={styles.container}>
-        <MainLogo style={styles.mainLogo}/>
-      
+      <MainLogo style={styles.mainLogo} />
+      <ScrollView style={styles.mainContents}>
+        <Pressable style={styles.roundBtn}>
+          <Text style={styles.btnText}>1</Text>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        backgroundColor: "#fff",
-      },
-    mainLogo : {
-        width: 200, // 로고 이미지의 너비
-        height: 50, // 로고 이미지의 높이
-        resizeMode: "contain", // 이미지를 화면에 맞게 조정
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  mainLogo: {
+    width: 200, // 로고 이미지의 너비
+    height: 50, // 로고 이미지의 높이
+    resizeMode: "contain", // 이미지를 화면에 맞게 조정
+  },
+  mainContents: {
+        paddingTop: 60
+  },
+  roundBtn: {
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    backgroundColor: "#e07e41",
+    justifyContent: "center", // 수직 방향 가운데 정렬
+    alignItems: "center", // 수평 방향 가운데 정렬
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+});
