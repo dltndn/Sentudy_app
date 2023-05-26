f99267e125abcfc440ea741c907009f4d9d4b9c1
dltndn <james98099@gmail.com>
2023-05-27 01:29:49 +0900

    style(web): Klip sdk 라이브러리 동작 함수 클래스 작성, 기본 틀 구축

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,133 +1,27 @@
 import React from "react";
 import { useState, useEffect } from "react";
-//@ts-ignore
-import { prepare, request as klipRequest, getResult as getKlipResult, getCardList } from "klip-sdk";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { KlipSdk } from "./utils/KlipSdk";
+import { MainPage } from "./pages/MainPage";
+import { create } from 'zustand'
+import { UseUserInfoType } from "./InterfaceComp";
 
-const MAIN_URL = "http://localhost:3000/"
+export const useUserInfo = create<UseUserInfoType>((set) => ({
+  address: null,
+  setAddress: (address: string) => set({address}),
+}))
 
 export default function App() {
-  const [result, setResult] = useState<any>("basic")
-
-  const testFunc = async () => {
-    try {
-      const response = await prepare.auth({ bappName: "Sentudy", successLink: MAIN_URL, failLink: MAIN_URL})
-      klipRequest(response.request_key)
-      let timerId = setInterval(async () => {
-        const data = await getKlipResult(response.request_key)
-        if (data.status === "completed") {
-          let klaytnAddress = data.result.klaytn_address
-          setResult(klaytnAddress)
-          return () => clearInterval(timerId);
-        }
-      }, 1000);
-    } catch (error) {
-      console.error(error);
-      setResult(error)
-    }
-  };
 
   return (
     <>
-      <button onClick={async () => await testFunc()}>test</button>
-      <>{JSON.stringify(result)}</>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
-// import { WagmiConfig, createClient, configureChains, useAccount } from "wagmi";
-// import { klaytn, Chain } from "wagmi/chains";
-// import { EthereumClient, w3mConnectors, w3mProvider } from "@web3modal/ethereum";
-// import { alchemyProvider } from "wagmi/providers/alchemy";
-// import { Web3Modal, Web3NetworkSwitch } from "@web3modal/react";
-// import { Web3Button } from "@web3modal/react";
-// import { Buffer } from "buffer";
-// import { create } from "zustand";
-// import { getMessage, updateMessage } from "./utils/interactWithBlockchain";
 
-// Buffer.from("anything", "base64");
-// window.Buffer = window.Buffer || require("buffer").Buffer;
-
-// const baobabRpcUrls = `https://klaytn-baobab-rpc.allthatnode.com:8551/${process.env.REACT_APP_ALLTHATNODE}`
-
-// const klaytn_baobab = {
-//   id: 1001,
-//   name: "Klaytn",
-//   network: "klaytn_baobab",
-//   nativeCurrency: {
-//     decimals: 18,
-//     name: "Klaytn",
-//     symbol: "KLAY",
-//   },
-//   rpcUrls: {
-//     default: {
-//       http: ["https://api.baobab.klaytn.net:8651"],
-//     },
-//     public: {
-//       http: ["https://api.baobab.klaytn.net:8651"],
-//     },
-//   },
-//   blockExplorers: {
-//     etherscan: {
-//       name: "BaobabKlaytnScope",
-//       url: "https://baobab.scope.klaytn.com/",
-//     },
-//     default: {
-//       name: "BaobabKlaytnScope",
-//       url: "https://baobab.scope.klaytn.com/",
-//     },
-//   },
-// } as const satisfies Chain
-
-// // klatrn_baobab
-// const chains = [klaytn_baobab];
-// const ALCHEMY_API_KEY = process.env.REACT_APP_ALCHEMY;
-// const WC_PROJECT_ID = process.env.REACT_APP_WC_PROJECT_ID;
-
-// const { provider } = configureChains(chains, [
-//   w3mProvider({ projectId: WC_PROJECT_ID ?? ""}),
-// ]);
-
-// const connector = w3mConnectors({
-//   chains: chains,
-//   version: 2,
-//   projectId: WC_PROJECT_ID ?? ""
-// })
-
-// const wagmiClient = createClient({
-//   autoConnect: true,
-//   connectors: connector,
-//   provider: provider,
-// });
-
-// const ethereumClient = new EthereumClient(wagmiClient, chains);
-
-// function App() {
-//   const testFunc = async () => {
-//     // const result = await updateMessage("Klaytn, Please...");
-//     const result = await getMessage()
-//     console.log(result);
-//   };
-
-//   return (
-//     <>
-//       <WagmiConfig client={wagmiClient}>
-//         <h1>sentudy</h1>
-//         <button onClick={async () => await testFunc()}>test btn</button>
-//         <Web3NetworkSwitch />
-//         <Web3Button icon="hide" label="지갑연결" balance="hide" />
-//       </WagmiConfig>
-//       <Web3Modal
-//         projectId={WC_PROJECT_ID ?? ""}
-//         ethereumClient={ethereumClient}
-//         themeVariables={{
-//           "--w3m-font-family": "Roboto, sans-serif",
-//           "--w3m-accent-color": "#eda951",
-//           "--w3m-button-border-radius": "60em",
-//           "--w3m-accent-fill-color": "#2a2c2e",
-//         }}
-//       />
-//     </>
-//   );
-// }
-
-// export default App;
---
+++ web/src/pages/MainPage.tsx
@@ -0,0 +1,6 @@
+
+export function MainPage() {
+    return <>
+    메인페이지
+    </>
+}
